7741d6d00bbce80e03ce26d4cfe2c8e0677924dd
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2022-01-03 02:15:23 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import Quote from './components/Quote';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
+
 
 function App() {
+
+  const [quote, setQuote] = useState({});
+  
+  const queryAPI = async () => {
+    const API = await fetch('https://breakingbadapi.com/api/quote/random?series=Breaking+Bad');
+    const quote = await API.json();
+    setQuote(quote[0]);
+  }
+  
+  useEffect(() => {
+    queryAPI()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Contenedor>
+        <Quote quote={quote}/>
+        <Boton
+          onClick={queryAPI}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Generate!
+        </Boton>
+      </Contenedor>
+    </>
   );
 }
 
---
+++ src/components/Quote.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import propTypes from 'prop-types';
+
+const ContenedorFrase = styled.div`
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width: 800px;
+    
+    @media (min-width: 992px) {
+        margin-top: 10rem;
+    }
+
+    h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.6rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+
+`;
+
+const Quote = ({quote}) => {
+
+    if(Object.keys(quote).length=== 0) return null;
+
+
+    return (
+        <ContenedorFrase>
+            <h2>{quote.quote}</h2>
+            <p>{quote.author}</p>
+        </ContenedorFrase>
+    )
+}
+
+Quote.prototype = {
+    quote: propTypes.object.isRequired()
+}
+
+export default Quote
